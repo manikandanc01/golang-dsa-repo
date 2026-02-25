3c2f5914cb876c45756ebc2f3977d79137bb9500
manikandan <cmanikandan680@gmail.com>
2026-02-25 22:19:12 +0530

    add counting the nodes problem

---
+++ Intermediate/Trees/counting_the_nodes.go
@@ -0,0 +1,63 @@
+package trees
+
+/*
+Problem Description
+Given the root of a tree A with each node having a certain value, find the count of nodes with more value than all its ancestors.
+Ancestor means that every node that occurs before the current node in the path from root to the node.
+
+Problem Constraints
+1 <= Number of Nodes <= 200000
+1 <= Value of Nodes <= 2000000000
+
+Input Format
+The first and only argument of input is a tree node.
+
+Output Format
+Return a single integer denoting the count of nodes that have more value than all of its ancestors.
+
+Example Input
+Input 1:
+     3
+Input 2:
+    4
+   / \
+  5   2
+     / \
+    3   6
+
+
+Example Output
+Output 1:
+ 1
+Output 2:
+ 3
+
+Example Explanation
+Explanation 1:
+ There's only one node in the tree that is the valid node.
+Explanation 2:
+ The valid nodes are 4, 5 and 6.
+*/
+
+func solveCountingTheNodes(A *treeNode) int {
+	return countNode(A, 0)
+}
+
+func countNode(A *treeNode, maxValue int) int {
+	if A == nil {
+		return 0
+	}
+
+	count := 0
+	if A.value > maxValue {
+		count += 1
+		maxValue = A.value
+	}
+
+	count += countNode(A.left, maxValue)
+	count += countNode(A.right, maxValue)
+
+	return count
+}
+
+// Tc is O(n), Sc is O(h) h is the height of the tree
